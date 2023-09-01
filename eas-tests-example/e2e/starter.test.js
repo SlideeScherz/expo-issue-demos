c5989b6bb46016cd2f6f7efd4e1c405353cb5712
Scott Scherzer <70346066+SlideeScherz@users.noreply.github.com>
2023-09-01 00:06:53 -0400

    hooks

--- eas-tests-example/e2e/starter.test.js
+++ eas-tests-example/e2e/starter.test.js
@@ -1,4 +1,4 @@
-describe('Example', () => {
+describe('Home screen', () => {
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -7,17 +7,12 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('"Click me" button should be visible', async () => {
+    await expect(element(by.id('click-me-button'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('shows "Hi!" after tapping "Click me"', async () => {
+    await element(by.id('click-me-button')).tap();
+    await expect(element(by.text('Hi!'))).toBeVisible();
   });
 });
